805b29e5105a8488312c9082e9507d08d169692e
h <huhuqaq@gmail.com>
2022-11-10 19:47:27 +0800

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Practice Problems - Algorithm
+
+- https://leetcode.com/
+- https://www.codewars.com/
+
+## Type
+
+- Arrays
+- Linked Lists
+- Stacks and Queues
+- Trees and Graphs
+- Hash table
+- Bit Manipulation
+- Math and Logic Puzzles
+- Recursion and Dynamic Programming
+- Sorting and Searching
---
+++ template.js
@@ -0,0 +1,31 @@
+/* 
+  Problems: xxx
+    - input: xxx
+    - output: xxx
+*/
+
+/*
+  # First solution: xxx
+  - time complexity: OOO
+  - space complexity: OOO
+*/
+
+/*
+  # Optimized solution: xxx
+  - time complexity: OOO
+  - space complexity: OOO
+*/
+
+/* 
+  # Good solution from others: xxx
+  - time complexity: OOO
+  - space complexity: OOO
+*/
+
+/* 
+  # Note: xxx
+*/
+
+/* 
+  ####### Tag: `xxx`
+*/
